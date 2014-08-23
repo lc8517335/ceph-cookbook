ea39c2a2204985daa0745cdfc83130c966fc5559
lc8517335 <lc8517335@163.com>
2014-08-23 14:02:42 +0800

    ceph-openstack-cookbook

--- ceph-openstack-connection/glance-config-ceph.rb
+++ ceph-openstack-connection/glance-config-ceph.rb
@@ -18,10 +18,8 @@ puts "**********************rbd_user:#{rbd_user}"
 if mon_nodes.nil?
   rbd_key = ""
   LOG.info("ceph storage cluster is not working,rbd key is empty#{rbd_key}")
-  puts "**********************mon_nodes_is_not_ok,rbd key is empty:#{rbd_key}"
 else
   rbd_key = mon_nodes[0]['ceph']['glance-secret']
-  puts "**********************rbd_key:#{rbd_key}"
 end
 
 template "/etc/ceph/ceph.client.#{rbd_user}.keyring" do
@@ -35,16 +33,19 @@ template "/etc/ceph/ceph.client.#{rbd_user}.keyring" do
       key: rbd_key
   )
 end
+node.set['openstack']['image']['api']['default_store'] = 'rbd'
+node.save
 
-execute "modify the glance-api.conf" do
-  command "sed -i 's/default_store = file/default_store = rbd/g' /etc/glance/glance-api.conf"
-  ignore_failure true
-  notifies :restart, 'service[glance-api-ceph]', :immediately
-end
+# execute "modify the glance-api.conf" do
+#   command "sed -i 's/default_store = file/default_store = rbd/g' /etc/glance/glance-api.conf"
+#   ignore_failure true
+#   notifies :restart, 'service[glance-api-ceph]', :immediately
+# end
 
 service 'glance-api-ceph' do
   service_name platform_options['image_api_service']
   supports status: true, restart: true
   action :enable
+  subscribes :restart, resources('template[/etc/ceph/ceph.conf]')
 end
 
